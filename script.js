ec4b03f285ee09f0dd3ce6dd825d2a9f39c7dfe6
Cluelesshint <connorwaggoner98@gmail.com>
2021-11-16 09:46:50 -0500

    add gameboard display

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
+const game = (() => {
+    const checkWin = () => {
+        const board = Gameboard.gameboard;
+    }
+    return { checkWin };
+})();
+
 const Gameboard = (() => {
     const gameboard = [
         [1, 2, 3],
         [4, 5, 6],
         [7, 8, 9]
-    ]
+    ];
+
 
     return { gameboard };
 })();
 
+const displayController = (() => {
+    const container = document.querySelector('.main-container');
+
+    const displayBoard = () => {
+
+    }
+})();
+
+const playerFactory = (name) => {
+    return { name };
+}
+
+const player1 = playerFactory('Player One');
+const player2 = playerFactory('Player Two');
